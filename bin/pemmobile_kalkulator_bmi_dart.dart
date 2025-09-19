eddb77c745acb763cea66670c6762384b6ce17fd
Nadya Hapsari Putri <nadya.hp14@gmail.com>
2025-09-19 09:59:30 +0700

    chore: remove old file after rename

--- bin/pemmobile_kalkulator_bmi_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:pemmobile_kalkulator_bmi_dart/pemmobile_kalkulator_bmi_dart.dart' as pemmobile_kalkulator_bmi_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${pemmobile_kalkulator_bmi_dart.calculate()}!');
-}
