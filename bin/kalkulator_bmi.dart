cc2ab5bcc951d04ff939b45b75b2c78043cc84a5
Nadya Hapsari Putri <nadya.hp14@gmail.com>
2025-09-19 09:13:32 +0700

    feat: Add BMI calculation with categories

---
+++ bin/kalkulator_bmi.dart
@@ -0,0 +1,29 @@
+import 'package:pemmobile_kalkulator_bmi_dart/pemmobile_kalkulator_bmi_dart.dart' as pemmobile_kalkulator_bmi_dart;
+
+// Fase 1: Input Validation
+void main() {
+  List<Map<String, dynamic>> riwayat = [];
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+
+  tampilkanRiwayat(riwayat);
+}
+
+void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
+  // Fase 2: BMI Calculation
+  double tinggiM = tinggiCm / 100;
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
+  }
+}
+
